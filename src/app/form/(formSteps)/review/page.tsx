52a392c2527ff7c75c2eebf4ec1d2802c0486c4e
Melanie <melanie.mazanec@gmail.com>
2025-08-15 11:23:38 -0400

    Fix loading spinner

--- src/app/form/(formSteps)/review/page.tsx
+++ src/app/form/(formSteps)/review/page.tsx
@@ -4,8 +4,10 @@ import FormProgressButtons from "@form/(formSteps)/components/FormProgressButton
 import { routeToNextStep, useFormProgressPosition } from "@form/_utils/formProgressRouting";
 import { Form } from "@trussworks/react-uswds";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { LoadingSpinner } from "../components/LoadingSpinner";
+import { getValue } from "@form/_utils/sessionStorage";
 
 const FormStep: React.FC = () => {
   const router = useRouter();
@@ -13,16 +15,39 @@ const FormStep: React.FC = () => {
   const { handleSubmit } = useForm<object>({
     defaultValues: {},
   });
+  const [loading, setLoading] = useState(true)
+  setTimeout(() => {
+    setLoading(false)
+  }, 2000);
+
   return (
     <div>
-      <Form
-        onSubmit={handleSubmit(() => {
-          routeToNextStep(router, formProgressPosition);
-        })}
-        className="maxw-full"
-      >
-        <FormProgressButtons />
-      </Form>
+      <LoadingSpinner loading={loading}>
+        <Form
+          onSubmit={handleSubmit(() => {
+            routeToNextStep(router, formProgressPosition);
+          })}
+          className="maxw-full form-container"
+        >
+          <div className="maxw-tablet" lang="cs">
+            <h2>Personal information</h2>
+            <dl>
+              <dt>First name</dt>
+              <dd>{getValue('firstName')}</dd>
+              <dt>Last name</dt>
+              <dd>{getValue('lastName')}</dd>
+            </dl>
+            <h2>Disclosures</h2>
+            <dl>
+              <dt>Had breakfast</dt>
+              <dd>{getValue('hadBreakfast')}</dd>
+              <dt>Breakfast tastiness</dt>
+              <dd>{getValue('breakfastWasTasty')}</dd>
+            </dl>
+            <FormProgressButtons />
+          </div>
+        </Form>
+      </LoadingSpinner>
     </div>
   );
 };
