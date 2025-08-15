523816adc18a8d535b3144ca285422da955745c9
Josephine Lee <josephine.lee@innovation.nj.gov>
2025-08-15 10:39:10 -0400

    Make button what should be a link

--- src/app/form/(formSteps)/components/FormProgressButtons.tsx
+++ src/app/form/(formSteps)/components/FormProgressButtons.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { formatFormProgressUrl, useFormProgressPosition } from "@form/_utils/formProgressRouting";
-import { Button, ButtonGroup, Link } from "@trussworks/react-uswds";
+import { Button, ButtonGroup } from "@trussworks/react-uswds";
+import { useRouter } from "next/navigation";
 
 const FormProgressButtons: React.FC = () => {
   const formProgressPosition = useFormProgressPosition();
+  const router = useRouter();
 
   return (
     <>
@@ -12,13 +14,19 @@ const FormProgressButtons: React.FC = () => {
         <div className="right-align">
           <ButtonGroup type="default">
             {formProgressPosition.previous !== null && (
-              <Link
+              <Button
                 key="previous"
-                href={formatFormProgressUrl(formProgressPosition.previous)}
+                onClick={() => {
+                  if (formProgressPosition.previous !== null) {
+                    router.push(formatFormProgressUrl(formProgressPosition.previous));
+                    router.refresh();
+                  }
+                }}
+                type="button"
                 className="usa-button usa-button--outline margin-top-0"
               >
                 Previous
-              </Link>
+              </Button>
             )}
             {formProgressPosition.next !== null && (
               <Button key="next" type="submit" className="margin-top-0">
